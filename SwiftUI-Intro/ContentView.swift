e8f531a4a37547efb8c8158da566259b87f18c3d
maitreebain <maitreebain@gmail.com>
2020-12-03 12:07:55 -0500

    intro to swiftUI:

---
+++ SwiftUI-Intro/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  SwiftUI-Intro
+//
+//  Created by Maitree Bain on 12/3/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import SwiftUI
+
+// [1, 2, 3].map(transform: { $0 + 1 })
+// [1, 2, 3].map { $0 + 1 }
+
+struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    var body: some View {
+        
+        VStack {
+            TextField("Search Articles", text: .constant(""))
+                .padding()
+                .background(colorScheme == .light ? Color.init(white: 0.9) : Color.init(white: 0.1) )
+                .cornerRadius(10)
+                .padding()
+            Picker("Category", selection: .constant(0)) {
+                Text("Food")
+                Text("Museums")
+                Text("Shops")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            List {
+                RowView()
+                RowView()
+                RowView()
+            }
+        }
+        .navigationBarTitle("Explore", displayMode: .inline)
+        .navigationBarItems(trailing:
+            HStack.init(spacing: 16) {
+                Button(action: {}) {
+//                        Text("Edit")
+                    Image(systemName: "pencil.circle")
+                }
+                Button(action: {}) {
+                        Image(systemName: "plus")
+                }
+            })
+    }
+}
+
+struct RowView: View {
+    var body: some View {
+        HStack.init(alignment: .center, spacing: 8) {
+            VStack.init(alignment: .leading, spacing: 20) {
+                Text("Hello, World!")
+                Text("Pancakes")
+            }
+            Button(action: { }) {
+                Text("Button")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            NavigationView {
+                ContentView()
+            }.environment(\.colorScheme, .light)
+            
+            NavigationView {
+                ContentView()
+            }.environment(\.colorScheme, .dark)
+        }
+    }
+}
