9f7b79b1f130ad3ee7bd2d02a96e9b4bef319690
maitreebain <maitreebain@gmail.com>
2020-12-17 13:09:47 -0500

    api call with combine

--- SwiftUI-Intro/Counter.swift
+++ SwiftUI-Intro/Counter.swift
@@ -7,10 +7,13 @@
 //
 
 import SwiftUI
+import Combine
 
 class CounterViewModel: ObservableObject {
     @Published private(set) var count = 0
     @Published var isModalPresented = false
+    @Published var fact: String?
+    var cancellable: Cancellable?
     
     func increment() {
         count += 1
@@ -27,6 +30,31 @@ class CounterViewModel: ObservableObject {
     func showModal() {
         isModalPresented = true
     }
+    
+    func showFact() {
+        withAnimation { fact = nil }
+        cancellable?.cancel()
+        //http://numbersapi.com/100/trivia
+        let url = URL(string: "http://numbersapi.com/\(count)/trivia")
+        
+//        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+//            guard let data = data else { return }
+//            let string = String.init(decoding: data, as: UTF8.self)
+//
+//            DispatchQueue.main.async {
+//                withAnimation { self.fact = string }
+//            }
+//        }.resume()
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: url!)
+            .map { data, _ in String(decoding: data, as: UTF8.self) }
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { string in
+                    withAnimation { self.fact = string }
+            })
+    }
 }
 
 struct CounterView: View {
@@ -34,38 +62,34 @@ struct CounterView: View {
     @State var isModalPresented = false
     @ObservedObject var viewModel: CounterViewModel
     var body: some View {
-        
         VStack{
+            //Label(fontSize: CGFloat(self.viewModel.count))
             HStack {
-                Button(action: {
-                    self.viewModel.decrement()
-                }) {
+                Button(action: { self.viewModel.decrement() }) {
                     Text("-")
-                    
                 }
-                
                 Text("\(viewModel.count)")
-                
-                Button(action: {
-                    self.viewModel.increment()
-                }) {
+                Button(action: { self.viewModel.increment() }) {
                     Text("+")
-                    
                 }
             }.padding()
             
-            Button(action: {
-                
-                self.viewModel.reset()
-            }) {
+            Button(action: { self.viewModel.reset() }) {
                 Text("Reset")
             }
             
-            Button(action: {
-                
-                self.isModalPresented = true
-            }) {Text("Show")}.padding()
+            Button(action: { self.isModalPresented = true }) {Text("Show")}.padding()
             
+            Button(action: { self.viewModel.showFact() }) {
+                Text("Show Fact")
+            }.padding()
+            
+//            if let fact = self.viewModel.fact {
+//            Text("\(fact)")
+//            }
+            self.viewModel.fact.map {
+                Text($0).transition(AnyTransition.opacity.combined(with: .slide))
+            }
         }
         .sheet(isPresented: self.$isModalPresented) {
             
@@ -96,6 +120,26 @@ struct ShowCountView: View {
             }
         })
     }
+}
+
+struct Label: UIViewRepresentable {
+    
+    var fontSize: CGFloat
+    let label = UILabel()
+    
+    func makeUIView(context: Context) -> UILabel {
+//        let label = UILabel()
+        label.text = "Hi"
+        return label
+    }
+    
+    func updateUIView(_ uiView: UILabel, context: Context) {
+        label.font = UIFont.systemFont(ofSize: fontSize)
+    }
+    
+    typealias UIViewType = UILabel
+    
+    
 }
 
 struct Counter_Previews: PreviewProvider {
